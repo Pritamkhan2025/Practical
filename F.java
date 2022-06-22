69db58593ed659547d2696c41e0b9ee873c13464
Pritamkhan2025 <107670962+Pritamkhan2025@users.noreply.github.com>
2022-06-22 13:52:00 +0530

    Add files via upload

---
+++ F.java
@@ -0,0 +1,8 @@
+class F{
+	public static void main (String[]args){
+
+		System.out.println("Welcome to the coding World");
+		System.out.println("Hello World");
+		System.out.println("My name is Khan");	
+	}
+}
