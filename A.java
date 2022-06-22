a70a4e4a200a2187f6e5e68c6d37aef1ccb3440f
Pritamkhan2025 <107670962+Pritamkhan2025@users.noreply.github.com>
2022-06-22 13:39:43 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,16 @@
+class A{
+ 	public static void main(String[]args){
+	
+	int x=27;
+	int y=5;
+	int a=x+y;
+	int s=x-y;
+	int m=x*y;
+	int d=x/y;
+	
+	System.out.println("addition:"+a);
+	System.out.println("substraction:"+s);
+	System.out.println("multiplication:"+m);
+	System.out.println("division:"+d);
+	}
+}
