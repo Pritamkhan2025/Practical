8f6cb6d72160301a76de318def52cecb7aa3a74d
PRITAM KHAN <107670962+Pritamkhan2025@users.noreply.github.com>
2022-08-05 02:14:40 +0530

    Add files via upload

---
+++ SequentialEg.java
@@ -0,0 +1,25 @@
+package thread;
+
+public class SequentialEg {
+	public void run() {
+		for(int i=0;i<=5;i++) {
+			try {
+				Thread.sleep(200);//the thread sleep/stop working
+			}
+			catch(InterruptedException e) {
+				System.out.println(e);
+			}
+			System.out.println(i);
+		}
+	}
+	public static void main(String args[]) {
+		SequentialEg t1=new SequentialEg();
+		SequentialEg t2=new SequentialEg();
+		SequentialEg t3=new SequentialEg();
+		t1.run();
+		t2.run();
+		t3.run();
+		
+	}//
+
+}
---
+++ ThreadclassStringname.java
@@ -0,0 +1,9 @@
+package thread;
+
+public class ThreadclassStringname {
+	public static void main(String args[]) {
+		Thread t=new Thread("first thread");
+		t.start();
+	}
+
+}
